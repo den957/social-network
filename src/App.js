83f88c20e95981b6b553b871e1db375163e63929
Dan 957 <denyshorban999@gamil.com>
2021-02-28 10:59:28 +0100

    Initialize code splitting with React Lazy and React Suspense

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import React, { useEffect, useState } from 'react'
 import Aside from './components/Aside/Aside'
-import ProfileContainer from './components/Profile/Profile'
+/*import ProfileContainer from './components/Profile/Profile'*/
 import Messages from './components/Messages/Messages'
-import Users from './components/Users/Users'
+/*import Users from './components/Users/Users'*/
 import Login from './components/Login/Login';
 import SettingsContact from './components/Settings/SettingsContact'
-import { Redirect, Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import { connect } from 'react-redux'
 import { initializeTC } from './redux/app.reducer';
 import Preloader from './components/Common/Preloader/Preloader';
@@ -15,8 +15,12 @@ import UsersAll from './components/Users/UsersAll';
 import Header from './components/Header/Header';
 import { MetaTags } from 'react-meta-tags'
 import { ReactTitle } from 'react-meta-tags'
+import { withSuspense } from './hoc/withSuspense';
 
-
+const ProfileContainerLazy = React.lazy(() => { return import('./components/Profile/Profile') })
+const UsersContainerLazy = React.lazy(() => { return import('./components/Users/Users') })
+const LoginContainerLazy = React.lazy(() => { return import('./components/Login/Login') })
+const UsersAllContainerLazy = React.lazy(() => { return import('./components/Users/UsersAll') })
 
 export const App = (props) => {
   useEffect(() => {
@@ -49,13 +53,12 @@ export const App = (props) => {
                   <Aside />
                 </div>
                 <div class='main__content'>
-                  <Route path={'/'} render={() => <Redirect to={'/profile'} />} />
-                  <Route path={'/profile/:userId?'} render={() => <ProfileContainer editModalStatus={editModalStatus} modalStatus={modalStatus} />} />
+                  <Route path={'/profile/:userId?'} render={withSuspense(() => { return <ProfileContainerLazy editModalStatus={editModalStatus} modalStatus={modalStatus} /> })} />
                   <Route path={'/messages'} render={() => <Messages />} />
-                  <Route exact path={'/users'} render={() => <Users />} />
+                  <Route exact path={'/users'} render={withSuspense(UsersContainerLazy)} />
                   <Route path={'/settings'} render={() => <SettingsContact />} />
-                  <Route path={'/login'} render={() => <Login />} />
-                  <Route path={'/users/all'} render={() => <UsersAll />} />
+                  <Route path={'/login'} render={withSuspense(LoginContainerLazy)} />
+                  <Route path={'/users/all'} render={withSuspense(UsersAllContainerLazy)} />
                 </div>
               </div>
             </div>
